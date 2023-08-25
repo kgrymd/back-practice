bcb369c404f5be5b9a6d570da6e97820ce749f2b
kgrymd <kgroymd@gmail.com>
2023-08-25 14:44:44 +0900

    change for user registration from front

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/csrf-token', function () {
+    return response()->json(['token' => csrf_token()]);
+});
+// ↑CSRFトークンを取得するためのルートを追加(8/25 14:11)
